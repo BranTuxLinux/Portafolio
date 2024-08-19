40a9abd3d714c34f81a6aaa1bf0840f71b46778c
Brandon Quintero <brandonquinterogit@gmail.com>
2024-08-19 16:27:01 -0500

    falta

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://BranTuxLinux.github.io/Portafolio'
   // resolve:{
   //   alias:{
   //     '@': path.resolve(__dirname, 'src')
